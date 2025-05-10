8ae3ec2615790101ac34d868f92fc83ab24e116f
ebcmacon <ebcmacon@gmail.com>
2025-05-09 20:23:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ app.get('/api/students', async (req, res) => {
 });
 
 app.listen(3001, () => console.log('Server running on port 3001'));
+
+
+app.get('/api/test-db', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json(result.rows);
+});
